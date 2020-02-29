2fa17c9f5f5788805c9b53ceffd6e67f2f257dab
William Loo <williamloo888@gmail.com>
2020-02-29 14:04:41 -0800

    upload script which splits one image into many smaller images

---
+++ some_shit_that_splits.py
@@ -0,0 +1,64 @@
+# USAGE
+# python classify.py --model apple_not_apple.model
+
+# import the necessary packages
+#from keras.preprocessing.image import img_to_array
+#from keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+import sys
+
+# construct the argument parse and parse the arguments
+
+
+"""
+sizes of array
+"""
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image_name", required=True,
+	help="image name")
+ap.add_argument("-n", "--rows", required=True,
+	help="num rows")
+ap.add_argument("-m", "--columns", required=True,
+	help="num columns")
+args = vars(ap.parse_args())
+
+#sizeX = img.shape[1]
+#sizeY = img.shape[0]
+img = cv2.imread(args["image_name"])
+height = img.shape[0]
+width = img.shape[1]
+
+
+#number of rows
+nRows = height//int(args["rows"])
+# Number of columns
+mCols = width//int(args["columns"])
+
+
+
+print(img.shape)
+
+
+for i in range(0,nRows):
+	for j in range(0, mCols):
+		roi = img[i*height//nRows:i*height//nRows + height//nRows ,j*width//mCols:j*width//mCols + width//mCols]
+		#cv2.imshow('rois'+str(i)+str(j), roi)
+		cv2.imwrite('patches/patch_'+str(i)+str(j)+".jpg", roi)
+
+
+cv2.waitKey()
+print("done.")
+
+
+
+
+
+
+
+
+
+
