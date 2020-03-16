66e62a127eb881736ac95d04350610c929c232f4
Ruhi Pudipeddi <ruhi@berkeley.edu>
2020-03-16 16:15:02 -0700

    script untested

--- nth_frame.py
+++ nth_frame.py
@@ -3,28 +3,12 @@ import numpy as np
 import os
 
 def process(vid_path, dim1, dim2, n):
-    out = crop_vid(vid_path, dim1, dim2)
-    out2 = save_nth_frames(out, n)
-    out3 = concat_all("./npy/", dim1, dim2)
+    out = save_nth_frames(out, n, dim1, dim2)
+    out2 = concat_all("./npy/", dim1, dim2)
     return out3
 
-# crops video to dim1, dim2
-def crop_vid(vid_path, dim1, dim2):
-    out = vid_path.split('.')[0] + "_cropped" + vid_path.split('.')[1]
-    v = cv2.VideoCapture(vid_path)
-    while True:
-    ret, frame = v.read()
-    if ret == True:
-        b = cv2.resize(frame,(dim1, dim2),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
-        out.write(b)
-    else:
-        break
-    v.release()
-    out.release()
-    cv2.destroyAllWindows()
-    return out
-
-def save_nth_frames(vid_path, n):
+# crops video to dim1, dim2 and saves all frames
+def save_nth_frames(vid_path, n, dim1, dim2):
     out = "./jpg/" + vid_path.split('.')[0] + "_every_" + str(n) + "_at_"
     v = cv2.VideoCapture(vid_path)
     i = 0
@@ -35,7 +19,10 @@ def save_nth_frames(vid_path, n):
         ret, frame = v.read()
         if ret == False:
             break
-        cv2.imwrite(out + str(i) + ".jpg", frame)
+        b = cv2.resize(frame,(dim1, dim2),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+        cv2.imwrite(out + str(i) + ".jpg", b)
+
+
         np_frame = cv2.imread(out + str(i) + ".jpg", 1)
         images.append(np_frame)
     all_frames = np.array(images)
