d24eaa7f0e809a432f4a0bd8285b14e26eae3f58
Oleksii Volkovskyi <volkovskyi@berkeley.edu>
2020-03-18 22:30:59 -0700

    fix reconstruct

--- image_utils.py
+++ image_utils.py
@@ -1,5 +1,5 @@
-import numpy as np
 import math
+import numpy as np
 def reconstruct(blocks, image_size):
     image = np.zeros(image_size)
     avg = np.zeros(image_size)
@@ -7,18 +7,16 @@ def reconstruct(blocks, image_size):
     bw = blocks.shape[2]
     bd = blocks.shape[3]
     for i in range(blocks.shape[0]):
-        fitH = math.ceil(image_size[0]/bh)
-        overH = image_size[0]%bh
-        fitW = math.ceil(image_size[1]/bw)
-        overW = image_size[1]%bw
+        fitH = math.ceil(image_size[0]/bh) #2
+        overH = image_size[0]%bh #2
+        fitW = math.ceil(image_size[1]/bw) #2
+        overW = image_size[1]%bw #2
 
-        h0 = bh*(i//fitW)-overH if bh*(i//fitW)+bh>image_size[0] else bh*(i//fitW)
+        h0 = image_size[0]-bh if bh*(i//fitW)+bh>image_size[0] else bh*(i//fitW)
         h1 = h0+bh
-        w0 = bw*(i%fitW)-overW if bw*(i%fitW)+bw>image_size[1] else bw*(i%fitW)
+        w0 = image_size[1]-bw if bw*(i%fitW)+bw>image_size[1] else bw*(i%fitW)
         w1 = w0+bw
-        
-        print('heights',h0,h1,'widths',w0,w1)
-        print(bh, bw, blocks.shape)
-        avg[h0:h1,w0:w1] += np.ones((bh,bw, bd))
-        image[h0:h1,w0:w1] += blocks[i]
+
+        avg[h0:h1,w0:w1,:] += np.ones((bh, bw, bd))
+        image[h0:h1,w0:w1,:] += blocks[i]
     return np.divide(image,avg)
