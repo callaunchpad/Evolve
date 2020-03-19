e252b51fd852d21e37ef7e3e58566cb9f7985b51
Mokshith Voodarla <mvoodarla@gmail.com>
2020-03-19 11:03:02 -0700

    make more bug fixes to ga, still not fully functional

--- run_ga.py
+++ run_ga.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     test_blocks = ims[150:]
     alg = GA(100, 10, (80, 80), train_im, train_blocks, test_im, test_blocks)
     for i in range(100):
-        print(alg.top_5_fitness())
+        # print(alg.top_5_fitness())
         alg.iterate()
