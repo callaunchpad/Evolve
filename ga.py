fa99dab545bc188b686ec2fd6b5600ac265cf4a3
Mokshith Voodarla <mvoodarla@gmail.com>
2020-03-18 22:14:36 -0700

    testing genetic algorithm changes

--- ga.py
+++ ga.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from keras_contrib.losses import DSSIMObjective
 import keras
+from keras import backend as K
 
 # Class for Genetic Algorithm
 class GA():
@@ -9,7 +10,7 @@ class GA():
     # Initialize genetic algorithm with follows parameters
     def __init__(self, pop_size, num_centroids, block_size, train_im, train_blocks, test_im, 
         test_blocks, fitness_func = 'SSIM', crossover_policy = 'uniform', 
-        selection_policy = 'roulette', mutation_proportion = 0.1, 
+        selection_policy = 'proportionate', mutation_proportion = 0.01, 
         mutation_policy = 'reroll'):
 
         self.num_centroids = num_centroids
@@ -23,16 +24,21 @@ class GA():
         self.pop_size = pop_size
         self.individuals = []
 
+        self.mutation_policy = mutation_policy
+        self.crossover_policy = crossover_policy
+        self.selection_policy = selection_policy
+        
         self.mutation_proportion = mutation_proportion
         self.fitness_func = fitness_func
         self.crossover_policy = crossover_policy
         self.selection_policy = selection_policy
         self.dssim = DSSIMObjective(kernel_size=3).__call__
         self.ssim = lambda im1, im2: 1 - self.dssim(im1, im2)
-        self.mse = keras.losses.mean_squared_error
+        #self.mse = lambda im1, im2: K.get_value(K.mean(keras.losses.mean_squared_error(im1, im2)))
+        self.mse = lambda im1, im2: ((im1 - im2) ** 2).flatten().mean()
         
         for i in range(pop_size):
-            self.individuals.append(Individual(num_centroids. block_size))
+            self.individuals.append(Individual(num_centroids, block_size))
 
         self.epoch = 0            
     
@@ -58,30 +64,28 @@ class GA():
         #FIXME - add more crossover policies
     
     # function that returns a fitnesss value for an individual based on a set of images
-    def fitness(self, ind, batch_size = 16):
-        used_indeces = np.random.choice(self.pop_size, batch_size)
+    def fitness(self, ind, batch_size = 2):
+        used_indeces = np.random.choice(self.train_im.shape[0], batch_size)
         fitness_val_per_im = []
 
-        def closest(bl):
-            close = self.mse(bl, self.train_blocks[i][0])
-            close_block = self.train_blocks[i][0]
-            for i in used_indeces:
-                for block in self.train_blocks[i]:
-                    error = self.mse(bl, block)
-                    if error < close:
-                        close = error
-                        close_block = block
-        
-        for i in used_indeces:
-            centroid_errors = 0
+        def closest_centroid(bl):
+            close = self.mse(bl, ind.centroids[0])
+            close_block = ind.centroids[0]
             for centroid in ind.centroids:
-                centroid_errors = centroid_errors + closest(centroid)
-            fitness_vals_per_im.append(centroid_errors)
-
-        return sum(fitness_val_per_im) / len(fitness_val_per_im)
+                error = self.mse(bl, centroid)
+                if error < close:
+                    close = error
+                    close_block = centroid
+            return centroid
         
-        return sum(fitness_vals_per_im) / len(fitness_vals_per_im)
-
+        centroid_error = 0
+        count = 0
+        for i in used_indeces:
+            for block in self.train_blocks[i]:
+                centroid_error = centroid_error + self.mse(closest_centroid(block), block)
+        
+        return -centroid_error / batch_size
+        
     # function that mutates individual for more biological feel to algorithm
     def mutate(self, ind):
         if self.mutation_policy == 'reroll':
@@ -104,24 +108,39 @@ class GA():
     
     # runs one epoch of the mating process
     def iterate(self):
+        print('mating...')
         offspring = self.mate(2)
+        print('calculating all fitness...')
         fitness_vals = [self.fitness(ind) for ind in offspring]
 
         if self.selection_policy == 'proportionate':
+            print('calculating proportions...')
             proportions = [val / sum(fitness_vals) for val in fitness_vals]
+            print('choosing at random now...')
             self.individuals = np.random.choice(offspring, len(self.individuals), proportions)
+            #print('mutating...')
+            #self.individuals = [self.mutate(ind) for ind in self.individuals]
 
         #FIXME - add more selection policies
     
-    def optimal_centroid():
+    def top_5_fitness(self):
+        temp = self.individuals
+        temp.sort(key = self.fitness)
+        fitness_sum = 0
+        for i in range(5):
+            fitness_sum = fitness_sum + self.fitness(temp[i])
+        return fitness_sum / 5
 
+    # returns the optimal set of centroids
+    def optimal_centroid(self):
+        return max(self.individuals, key = self.fitness)
 
 # An individual defined by a set centroids (blocks)
 class Individual():
     def __init__(self, num_centroids = 100, block_size = (5, 5), centroids = None):
-        if centroids:
-            self.centroids = centroids
-            self.num_centroids = centroids.shape[0]
+        if centroids is not None:
+            self.centroids = np.array(centroids)
+            self.num_centroids = self.centroids.shape[0]
         else: 
             self.num_centroids = num_centroids
             self.centroids = []
