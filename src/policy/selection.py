7bfb02c30b3b53aad5955804da9c8fa95441781c
Mokshith Voodarla <mvoodarla@gmail.com>
2020-04-13 14:58:05 -0700

    add selection policies

--- src/policy/selection.py
+++ src/policy/selection.py
@@ -5,15 +5,13 @@ fitness/selection/crossover/mutation.py
 - implement gradient mutation + single-point set crossover, 2x2x1 block size
 - later: nested vector quantization, sepearate channel quantization
 '''
-def proportionate(fitness_vals, individuals, crossover, mutate):
+def proportionate(fitness_vals):
     proportions = [val / sum(fitness_vals) for val in fitness_vals]
-    parents_to_keep = np.random.choice(individuals, len(individuals) // 2, proportions)
+    indeces = [i for i in range(len(fitness_vals))]
+    parents_to_keep = np.random.choice(indeces, len(indeces) // 2, proportions)
 
     offspring = []
     while len(offspring) < len(individuals) // 2:
         pts = np.random.randint(low=0, high=len(parents_to_keep), size=2)
-        child1, child2 = crossover(parents_to_keep[pts[0]], parents_to_keep[pts[1]])
-        offspring.append(mutate(child1))
-        offspring.append(mutate(child2))
-
-    return np.hstack([parents_to_keep, offspring])
+        offspring.append((pts[0], pts[1]))
+    return offspring
