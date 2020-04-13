222a639c702dd7bbc2bade27b2f53a880d29b816
Mokshith Voodarla <mvoodarla@gmail.com>
2020-04-13 14:42:48 -0700

    add policies to new files

--- src/policy/mutation.py
+++ src/policy/mutation.py
@@ -1,6 +1,17 @@
+import numpy as np
+from scipy.ndimage import gaussian_filter
 '''
 fitness/selection/crossover/mutation.py
 - abstract away the policies into respective files (for ease of experimentation)
 - implement gradient mutation + single-point set crossover, 2x2x1 block size
 - later: nested vector quantization, sepearate channel quantization
 '''
+def reroll(codeblocks):
+    replace = (np.random.random(codeblocks.shape) < self.mutation_proportion) * 1
+    new_codeblocks = (codeblocks * (1 - replace) + np.random.randint(0, 256, codeblocks.shape) * replace)
+    return new_codeblocks
+
+def reroll_gaussian(codeblocks):
+    re_rolled = reroll(codeblocks)
+    g_rerolled = gaussian_filter(re_rolled, sigma=(0, 1, 1, 0))
+    return np.array(g_rerolled)
--- src/policy/selection.py
+++ src/policy/selection.py
@@ -1,6 +1,19 @@
+import numpy as np
 '''
 fitness/selection/crossover/mutation.py
 - abstract away the policies into respective files (for ease of experimentation)
 - implement gradient mutation + single-point set crossover, 2x2x1 block size
 - later: nested vector quantization, sepearate channel quantization
 '''
+def proportionate(fitness_vals, individuals, crossover, mutate):
+    proportions = [val / sum(fitness_vals) for val in fitness_vals]
+    parents_to_keep = np.random.choice(individuals, len(individuals) // 2, proportions)
+
+    offspring = []
+    while len(offspring) < len(individuals) // 2:
+        pts = np.random.randint(low=0, high=len(parents_to_keep), size=2)
+        child1, child2 = crossover(parents_to_keep[pts[0]], parents_to_keep[pts[1]])
+        offspring.append(mutate(child1))
+        offspring.append(mutate(child2))
+
+    return np.hstack([parents_to_keep, offspring])
