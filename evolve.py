827ace571d0e0a2c4f4906a50b65ae8944dc55a9
Nathan Nakamitsu <nnakamitsu@berkeley.edu>
2020-04-19 14:54:33 -0700

    Evolve and Reblock

--- evolve.py
+++ evolve.py
@@ -14,3 +14,95 @@ for decompression:
 - generate "filename".[jpg/mp4]
 note: later we'll merge compress and decompress into one exectuable so that we can just run "evolve -c image.jpg" for compression and "evolve -d code.ev" for decompression on the command-line
 '''
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+import os
+from coding import *
+import time
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-c', "--compress", action='store_true')
+ap.add_argument('-d', "--decompress", action='store_true')
+ap.add_argument("-v", "--video", required=False,default=None,
+    help="video path")
+ap.add_argument("-i", "--image", required=False,default=None,
+    help="image path")
+
+args = vars(ap.parse_args())
+
+CODEVECTOR_PATH = "/path_to_codevector"
+
+def run():
+    if not args["compress"] and not args["decompress"]:
+        print("missing argument -c or -d")
+        raise ValueError('missing argument -c or -d"')
+    if args["compress"] and args["decompress"]:
+        print("cannot be both compress and decompress")
+        raise ValueError('cannot be both compress and decompress')
+    if args["video"] and args["image"]:
+        print("cannot be both video and image")
+        raise ValueError('cannot be both video and image')
+    if not args["video"] and not args["image"]:
+        print("need video or image")
+        raise ValueError('need video or image')
+
+    if args["compress"]:
+        if args["image"]:
+            tic = time.perf_counter()
+            ret = encode(args["image"])
+            toc = time.perf_counter()
+
+            print(f"Encoded in {toc - tic:0.4f} seconds")
+            #return ret #should be npy or ev?
+
+        elif args["video"]:
+            tic = time.perf_counter()
+            encoded_movie = [] #array of npys
+            video_path = args["video"]
+            v = cv2.VideoCapture(video_path)
+            while True:
+                ret, frame = v.read()
+                if ret == True:
+                    #b = cv2.resize(frame,(480, 360),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+                    encoded_movie.append(encode(frame))
+                else:
+                    break
+            toc = time.perf_counter()
+
+            v.release()
+            cv2.destroyAllWindows()
+
+            print(f"Encoded in {toc - tic:0.4f} seconds")
+
+            np.save("encoded_movie.ev", encoded_movie)
+
+
+    elif args["decompress"]:
+
+        if args["image"]:
+            tic = time.perf_counter()
+            ret = decode(args["image"])
+            toc = time.perf_counter()
+
+            print(f"Decoded in {toc - tic:0.4f} seconds")
+            #cv2.imwrite("decoded.jpg", ret) #should be .jpg
+
+        elif args["video"]:
+            tic = time.perf_counter()
+            fourcc = VideoWriter_fourcc(*'MP4V')
+            out = VideoWriter("decoded_movie.mp4", fourcc, 30.0, (640,480)) #makes mp4
+            #out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+
+            video_input = np.load("encoded_movie.ev")
+            for i in range(len(video_input)):
+                out.write(decode(video_input[i]))
+
+            toc = time.perf_counter()
+            out.release()
+
+            print(f"Decoded in {toc - tic:0.4f} seconds")
+
+if __name__ == "__main__":
+    run()
--- src/reblock.py
+++ src/reblock.py
@@ -3,3 +3,107 @@
 - iterate through image directory and regenerate the blocks in blocks/
 - will use blockify method in utils
 '''
+
+import os
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-H", "--rows", required=True,
+    help="num rows")
+ap.add_argument("-W", "--columns", required=True,
+    help="num columns")
+ap.add_argument("-D", "--depth", required=True,
+    help="num channels")
+
+ap.add_argument("-p", "--padding", required=False,default=0,
+    help="padding setting (0 - zero, 1 - edge, 2 - reflect, 3 - Oleksii)")
+
+args = vars(ap.parse_args())
+
+blockRows = int(args["rows"])
+blockColumns = int(args["columns"])
+blockDepth = int(args["depth"])
+
+
+def blockify_single_frame(filename,H=blockRows,W=blockColumns):
+
+    img = cv2.imread("data/images/" + filename)
+
+    #Actual height & width of image
+    height = img.shape[0]
+    width = img.shape[1]
+
+    #Block Dimensions
+    blockRows = H
+    blockColumns = W
+
+    #Padding setting
+    padding = int(args['padding'])
+
+    if padding == 0: #zero padding
+        img2 = cv2.copyMakeBorder(img, 0, blockRows - height % blockRows, 0, blockColumns - width % blockColumns, cv2.BORDER_CONSTANT, (0,0,0,0))
+    elif padding == 1: #edge copying padding
+        img2 = cv2.copyMakeBorder(img, 0, blockRows - height % blockRows, 0, blockColumns - width % blockColumns, cv2.BORDER_REPLICATE)
+    elif padding == 2: #edge reflection padding
+        img2 = cv2.copyMakeBorder(img, 0, blockRows - height % blockRows, 0, blockColumns - width % blockColumns, cv2.BORDER_REFLECT)
+    elif padding == 3: #oleksii padding
+        img2 = img
+
+    # cv2.imwrite(final, "data/blocks/_blocks" + filename)
+
+
+    # Number of rows (Of Blocks)
+    nRows = height//blockRows
+    # Number of columns (Of Blocks)
+    mCols = width//blockColumns
+
+    #this for loop does not pad at the moment
+    #added one to account for padding the rest'
+    #idea: concatenate horizontally the distance from len - remainder the edge pixel
+    if padding in range(3):
+        final = []
+        for i in range(0,nRows + 1):
+            for j in range(0, mCols + 1):
+
+                roi = img2[i*blockRows:i*blockRows + blockRows ,j*blockColumns:j*blockColumns + blockColumns]
+
+                #cv2.imwrite("debug_output/"+str(frame_num)+'_blocks/block'+str(i)+str(j)+".jpg", roi)
+                final.append(roi)
+
+        np.save("data/blocks/blocks_" + filename,final)
+
+    else:
+        final = []
+        for i in range(0,nRows + 1):
+            for j in range(0, mCols + 1):
+                if i == nRows and j == mCols:
+                    roi = img[i*blockRows - (blockRows - (height % blockRows)):, j*blockColumns - (blockColumns - (width % blockColumns)):]
+                elif i == nRows:
+                    roi = img[i*blockRows - (blockRows - (height % blockRows)):, j*blockColumns:j*blockColumns + blockColumns]
+                elif j == mCols:
+                    roi = img[i*blockRows:i*blockRows + blockRows, j*blockColumns - (blockColumns - (width % blockColumns)):]
+                else:
+                    roi = img[i*blockRows:i*blockRows + blockRows, j*blockColumns:j*blockColumns + blockColumns]
+
+                # cv2.imwrite("debug_output/"+str(frame_num)+'_blocks/block'+str(i)+str(j)+".jpg", roi)
+
+                final.append(roi)
+        np.save("data/blocks/blocks_" + filename,final)
+
+
+if __name__ == "__main__":
+
+    if not os.path.exists("data/blocks"):
+        os.makedirs("data/blocks")
+
+    if not os.path.exists("data/images"):
+        os.makedirs("data/images")
+        print("created images directory, please put in images")
+
+    for filename in os.listdir("data/images"):
+        if filename.endswith(".jpg"):
+            blockify_single_frame(filename)
